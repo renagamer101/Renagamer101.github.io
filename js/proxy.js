e6cd152f9497a69e966c939f37e7382feba8d77e
System32-Dev <83769295+System32-Dev@users.noreply.github.com>
2023-04-26 21:06:19 -0400

    Delete js directory

--- js/proxy.js
+++
@@ -1,8 +0,0 @@
-navigator.serviceWorker.register("/sw.js", {scope:__uv$config.prefix})
-
-Object.defineProperty(window, "toProxy", {
-    value: url => {
-        let loc = __uv$config.prefix + __uv$config.encodeUrl(url);
-        location.replace(loc);
-    }
-})
