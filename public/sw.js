9ee1ec5df9de93bc8aab6c6d373c601ac4157f7c
System32-Dev <83769295+System32-Dev@users.noreply.github.com>
2023-05-02 17:03:52 -0400

    Delete public directory

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('/uv/uv.sw.js');
-
-const UV = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => {
-  	if (event.request.url.startsWith(location.origin + '/service/')) event.respondWith(UV.fetch(event));
-});
