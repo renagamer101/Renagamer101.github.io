a2591aa0831434c5b3cf3731fb5dd217c6ceb341
Rena <krishpatel2099@gmail.com>
2023-09-09 16:15:22 -0400

    wfw

--- js/home.js
+++ js/home.js
@@ -1,15 +1,15 @@
-// function initTitle() {
-//     const title = document.querySelector(".title p");
+function initTitle() {
+    const title = document.querySelector(".title p");
     
-//     title.addEventListener("click", e => {
-//         if (localStorage.getItem("theme") == "default" || localStorage.getItem("theme") == null) localStorage.setItem("theme","moon",); 
+    title.addEventListener("click", e => {
+        if (localStorage.getItem("theme") == "default" || localStorage.getItem("theme") == null) localStorage.setItem("theme","moon",); 
       
-//         else localStorage.setItem("theme", "default");
-//         loadTheme(localStorage.getItem("theme"));
-//         loadTheme(localStorage.getItem("theme"), top.document);
-//     })
-// }
+        else localStorage.setItem("theme", "default");
+        loadTheme(localStorage.getItem("theme"));
+        loadTheme(localStorage.getItem("theme"), top.document);
+    })
+}
 
-// addEventListener("DOMContentLoaded", () => {
-//     initTitle();
-// })
+addEventListener("DOMContentLoaded", () => {
+    initTitle();
+})
