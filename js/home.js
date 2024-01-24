6c6150cbde7a5b1a90ea32f8ad4d20c04f500861
Rena <96131117+renagamer101@users.noreply.github.com>
2024-01-24 08:32:57 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 function initTitle() {
     const title = document.querySelector(".title p");
     
-    title.addEventListener("click", e => {
-        if (localStorage.getItem("theme") == "default" || localStorage.getItem("theme") == null) localStorage.setItem("theme","moon",); 
+    // title.addEventListener("click", e => {
+    //     if (localStorage.getItem("theme") == "default" || localStorage.getItem("theme") == null) localStorage.setItem("theme","moon",); 
       
         else localStorage.setItem("theme", "default");
         loadTheme(localStorage.getItem("theme"));
